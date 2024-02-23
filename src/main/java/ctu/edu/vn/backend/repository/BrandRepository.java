ba8636b89a99172223232ccc3c506f57dfa50df1
anhb2012060 <anhb2012060@gmail.com_>
2024-02-23 21:46:56 +0700

    feat: add interface class for Brand

---
+++ src/main/java/ctu/edu/vn/backend/repository/BrandRepository.java
@@ -0,0 +1,7 @@
+package ctu.edu.vn.backend.repository;
+
+import ctu.edu.vn.backend.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+}
