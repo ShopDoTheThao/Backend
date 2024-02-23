ed8913886f1740d8d22f16fc3c0ccad41ec17218
anhb2012060 <anhb2012060@gmail.com_>
2024-02-23 21:49:41 +0700

    feat: add interface class for Shopping Cart

--- src/main/java/ctu/edu/vn/backend/repository/ShoppingCartRepository.java
+++ src/main/java/ctu/edu/vn/backend/repository/ShoppingCartRepository.java
@@ -4,4 +4,5 @@ import ctu.edu.vn.backend.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+
 }
