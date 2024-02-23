1f8a98d9e490fde8c27ec37f419b639f67a5b230
anhb2012060 <anhb2012060@gmail.com_>
2024-02-23 09:58:19 +0700

    Class Product

---
+++ src/main/java/ctu/edu/vn/backend/entity/Product.java
@@ -0,0 +1,13 @@
+package ctu.edu.vn.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+}
