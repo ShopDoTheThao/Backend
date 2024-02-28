2bb4e25fd9afb86b6320afe7437be986fa8973c6
TienDuong <duongb2012073@gmail.com>
2024-02-28 09:42:01 +0700

    add class AdressRepository

--- src/main/java/ctu/edu/vn/backend/repository/CustomerStatusRepository.java
+++ src/main/java/ctu/edu/vn/backend/repository/CustomerStatusRepository.java
@@ -3,5 +3,5 @@ package ctu.edu.vn.backend.repository;
 import ctu.edu.vn.backend.entity.CustomerStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICustomerStatusRepository extends JpaRepository<CustomerStatus, Long> {
+public interface CustomerStatusRepository extends JpaRepository<CustomerStatus, Long> {
 }
--- src/main/java/ctu/edu/vn/backend/repository/PersonRepository.java
+++ src/main/java/ctu/edu/vn/backend/repository/PersonRepository.java
@@ -3,5 +3,5 @@ package ctu.edu.vn.backend.repository;
 import ctu.edu.vn.backend.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IPersonRepository extends JpaRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
