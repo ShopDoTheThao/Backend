502316c921274953939e4c0d2a99492b65b9ef1c
TienDuong <duongb2012073@gmail.com>
2024-02-28 08:51:32 +0700

    add class District

---
+++ src/main/java/ctu/edu/vn/backend/entity/District.java
@@ -0,0 +1,19 @@
+package ctu.edu.vn.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "district")
+public class District {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
