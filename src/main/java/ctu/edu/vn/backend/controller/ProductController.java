5ef66b32f624ed84244bbc939c3319818c505b8e
TienDuong <duongb2012073@gmail.com>
2024-03-12 21:02:42 +0700

    init package controller

---
+++ src/main/java/ctu/edu/vn/backend/controller/ProductController.java
@@ -0,0 +1,9 @@
+package ctu.edu.vn.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+}
