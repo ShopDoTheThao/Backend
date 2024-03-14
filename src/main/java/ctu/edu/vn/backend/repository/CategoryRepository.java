265d62820569a2dc10bcbe8af0f10b7be5dbe056
anhb2012060 <anhb2012060@gmail.com_>
2024-03-14 23:31:01 +0700

    Add class reponsitory Category

--- src/main/java/ctu/edu/vn/backend/repository/CategoryRepository.java
+++ src/main/java/ctu/edu/vn/backend/repository/CategoryRepository.java
@@ -3,5 +3,10 @@ package ctu.edu.vn.backend.repository;
 import ctu.edu.vn.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findByNameIgnoreCase(String name);
 }
