cd30bf9a2ad651685031f5e191577fa3a30aec7d
anhb2012060 <anhb2012060@gmail.com_>
2024-03-14 23:30:25 +0700

    Add class exception Category

---
+++ src/main/java/ctu/edu/vn/backend/error/entityException/CategoryException.java
@@ -0,0 +1,7 @@
+package ctu.edu.vn.backend.error.entityException;
+
+public class CategoryException extends RuntimeException{
+    public CategoryException(String message) {
+        super(message);
+    }
+}
