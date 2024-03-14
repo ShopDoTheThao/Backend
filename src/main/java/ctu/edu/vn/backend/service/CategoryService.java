66e33cb6557f8e2fc73ce0101f9b0c23b1dc7d7e
anhb2012060 <anhb2012060@gmail.com_>
2024-03-14 23:31:11 +0700

    Add class service Category

--- src/main/java/ctu/edu/vn/backend/service/CategoryService.java
+++ src/main/java/ctu/edu/vn/backend/service/CategoryService.java
@@ -1,4 +1,27 @@
 package ctu.edu.vn.backend.service;
 
+import ctu.edu.vn.backend.dto.BrandDto;
+import ctu.edu.vn.backend.dto.CategoryDto;
+import ctu.edu.vn.backend.entity.Brand;
+import ctu.edu.vn.backend.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryService {
+    List<Category> findCategoryByName(String name);
+
+    Category save(CategoryDto dto);
+
+    List<Category> findAll();
+
+    Page<Category> findAll(Pageable pageable);
+
+    Optional<Category> findById(Long id);
+
+    void deleteById(Long id);
+
+    Category update(Long id, Brand entity);
 }
